b459377c6eb317b355681248fc142b3fea82ef8f
LAUTARO ORTIZ <86262695+camilayluna@users.noreply.github.com>
2024-03-21 19:49:33 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,27 @@
+function guardar() {
+    // Código para guardar los datos del formulario en un excel
+}
+
+function buscar() {
+    // Código para buscar formularios
+}
+
+function limpiar() {
+    // Código para limpiar el texto de búsqueda
+}
+
+function hechoNoHecho() {
+    // Código para marcar el formulario como hecho o no hecho
+}
+
+function descargarMensual() {
+    // Código para descargar el excel con todos los datos mensuales
+}
+
+function excelHoy() {
+    // Código para descargar el excel con los datos de hoy
+}
+
+function borrar() {
+    // Código para borrar todos los formularios
+}
